c6d294cef3d9392894f6d6f28df8797f5c43fb21
silvayuni <silvayuni@users.noreply.github.com>
2014-05-03 18:49:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,3 +5,5 @@ $startUrl = "http://www.detik.com/";
 
 // anchor title "//div[@class='yt-lockup-content']//h3[@class='yt-lockup-title']/a/text()"
 // anchor title link "//div[@class='yt-lockup-content']//h3[@class='yt-lockup-title']/a/@href"
+//read the xml source as string
+$str = file_get_contents ("imoracle.xml")
