08ab176b99d40da7a4fe18e448737874911f615e
ithand <idand46@gmail.com>
2014-05-03 19:31:18 +0700

    Update explode.php

--- explode.php
+++ explode.php
@@ -14,3 +14,34 @@ for($x=0; $x<$count; $x++){
 
 
 ?>
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>DETIK</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<div class="container">
+	<h1>Detik berita bola dalam dan luar negri</h1>
+	<?php
+	$output = '';
+	foreach($newArray as $new){
+		/*echo '<div class="video">';
+		echo '<img src ="' .$new['imageSrc'] . '" alt="detik image">';
+		echo '<a href="' . $new['link'] . '"><h4>' . $new['title'] . '</h4></a>';*/
+
+	
+	echo '<table border="0" cellpadding="1" cellspacing="2">';
+	echo '<tr class="row">';
+	echo '<td class="col"><a href="' .$new['link']. '"><h5>' .$new['title']. '</h5></a></td>';
+	//echo '<td class="own"><a href="' .$new['ownerLink']. '"><h6>' .$new['owner']. '</h6></a></td>';
+	//cho '<td class="dur">' .$new['duration']. '</td>';
+	echo '</tr>';
+	echo '</table>';
+	}
+
+	?>
+	</div>
+</body>
+</html>
